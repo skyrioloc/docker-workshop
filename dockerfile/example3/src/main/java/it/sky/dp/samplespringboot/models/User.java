dd55859bec0f4001aca9c9efbc2477f2a2e0d4fa
skyrioloc <carmelo.riolo@skytv.it>
2018-07-11 23:24:40 +0200

    Removed target dir && added .gitignore

---
+++ dockerfile/example3/src/main/java/it/sky/dp/samplespringboot/models/User.java
@@ -0,0 +1,43 @@
+package it.sky.dp.samplespringboot.models;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public User(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ dockerfile/example3/src/main/java/it/sky/dp/samplespringboot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package it.sky.dp.samplespringboot.repository;
+
+import it.sky.dp.samplespringboot.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findById(String id);
+
+}
