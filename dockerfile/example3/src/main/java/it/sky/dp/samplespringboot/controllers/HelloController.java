83451e4d0f164439b60c6a0a0b451b71cfadd3d6
skyrioloc <carmelo.riolo@skytv.it>
2018-07-11 09:21:30 +0200

    Added Spring Boot Example

---
+++ dockerfile/example3/src/main/java/it/sky/dp/samplespringboot/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package it.sky.dp.samplespringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+}
