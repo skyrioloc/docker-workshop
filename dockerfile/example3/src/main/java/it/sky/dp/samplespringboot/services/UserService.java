d03ea565287c9cca93b25a38a150ea04dcde0ff1
skyrioloc <carmelo.riolo@skytv.it>
2018-07-13 00:30:32 +0200

    Changed Project Structure

--- dockerfile/example3/src/main/java/it/sky/dp/samplespringboot/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package it.sky.dp.samplespringboot.services;
-import it.sky.dp.samplespringboot.models.User;
-
-public interface UserService {
-
-    User getUser(Integer id);
-    void createUser(User user);
-
-}
